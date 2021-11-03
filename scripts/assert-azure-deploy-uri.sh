10029171dbde7b2223b72cfa94c8f8076783f1ef
Mattias Kindborg <73967018+mattias-kindborg-at-work@users.noreply.github.com>
2021-11-03 12:54:32 +0100

    test: azure deploy button uri (#12)

---
+++ scripts/assert-azure-deploy-uri.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+if [[ $# -ne 1 ]] ; then
+  echo "Error: Unsupported number of arguments"
+  echo
+  echo "USAGE:"
+  echo "    assert-azure-deploy-uri.sh <example name> "
+  echo
+  echo "WHERE:"
+  echo "    example name    The name of the example."
+  echo
+
+  exit 1
+fi
+
+example_name=$1
+
+content=$(cat README.md)
+regex='https://portal.azure.com/#create/Microsoft.Template/uri/([^)]*)'
+
+if ! [[ $content =~ $regex ]]; then
+  echo "The expected Azure Deploy button was not found."
+  exit 1
+fi
+
+actual_uri="${BASH_REMATCH[1]}"
+expected_uri="https%3A%2F%2Fraw.githubusercontent.com%2FAxisCommunications%2Facap-integration-examples-azure%2Fmain%2F$example_name%2Fmain.json"
+
+if [[ $actual_uri != $expected_uri ]]; then
+   echo "The expected ARM template URI was not found."
+   exit 1
+fi
+
+echo "Success!"
