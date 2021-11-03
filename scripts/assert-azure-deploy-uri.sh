d630807d67fe834c1b76aaee649e51745c7314db
Mattias Kindborg <mattiask@axis.com>
2021-11-03 12:42:28 +0100

    wip

--- scripts/assert-azure-deploy-uri.sh
+++ scripts/assert-azure-deploy-uri.sh
@@ -15,13 +15,12 @@ if [[ $# -ne 1 ]] ; then
 fi
 
 example_name=$1
-file_path=$2
 
-file_content=$(cat README.md)
+content=$(cat README.md)
 regex='https://portal.azure.com/#create/Microsoft.Template/uri/([^)]*)'
 
-if ! [[ $file_content =~ $regex ]]; then
-  echo "The expected Azure Deploy button was not found in '$file_path'."
+if ! [[ $content =~ $regex ]]; then
+  echo "The expected Azure Deploy button was not found."
   exit 1
 fi
 
@@ -29,7 +28,7 @@ actual_uri="${BASH_REMATCH[1]}"
 expected_uri="https%3A%2F%2Fraw.githubusercontent.com%2FAxisCommunications%2Facap-integration-examples-azure%2Fmain%2F$example_name%2Fmain.json"
 
 if [[ $actual_uri != $expected_uri ]]; then
-   echo "The expected ARM template URI was not found in '$file_path'."
+   echo "The expected ARM template URI was not found."
    exit 1
 fi
 
